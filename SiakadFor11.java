1c9e38c4337b1be14ddd42d6cb3398a259b14ca8
Rizkihadiono <rizkihadiono95@gmail.com>
2024-10-10 13:04:48 +0700

    https://github.com/RizkiHadiono/daspro-jobsheet7.git

---
+++ SiakadFor11.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class SiakadFor11 {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        double nilai, tertinggi = 0, terendah = 100;
+        for (int i = 1; i <= 10; i++) {
+            
+        }
+    }
+}
