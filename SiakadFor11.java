1b94f8ec6c9e4be066dec07497e792d81e1d6971
Rizkihadiono <rizkihadiono95@gmail.com>
2024-10-10 15:42:30 +0700

    https://github.com/RizkiHadiono/daspro-jobsheet7.git

--- SiakadFor11.java
+++ SiakadFor11.java
@@ -9,7 +9,16 @@ public class SiakadFor11 {
 
         double nilai, tertinggi = 0, terendah = 100;
         for (int i = 1; i <= 10; i++) {
-            
+            System.out.println("Masukkan nilai mahasiswa ke-" + i + ": ");
+            nilai = sc.nextDouble();
+            if (nilai > tertinggi) {
+                tertinggi = nilai;
+            }
+            if (nilai < terendah) {
+                terendah = nilai;
+            }
         }
+        System.out.println("Nilai tertinggi: " + tertinggi);
+        System.out.println("Nilai terendah " + terendah);
     }
 }
