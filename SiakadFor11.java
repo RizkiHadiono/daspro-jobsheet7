1f7529bd7abc7111be5eb9040a0c4bbf09f926d0
Rizkihadiono <rizkihadiono95@gmail.com>
2024-10-16 19:11:12 +0700

    https://github.com/RizkiHadiono/daspro-jobsheet7.git

--- SiakadFor11.java
+++ SiakadFor11.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class SiakadFor11 {
@@ -8,17 +7,35 @@ public class SiakadFor11 {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
+        int jumlahLulus = 0, jumlahTidakLulus = 0;
         for (int i = 1; i <= 10; i++) {
             System.out.println("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
+            
+            // Mengecek nilai tertinggi
             if (nilai > tertinggi) {
                 tertinggi = nilai;
             }
+            
+            // Mengecek nilai terendah
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            
+            // Mengecek apakah mahasiswa lulus atau tidak
+            if (nilai >= 60) {
+                jumlahLulus++;
+            } else {
+                jumlahTidakLulus++;
+            }
         }
+        
+        // Menampilkan hasil nilai tertinggi dan terendah
         System.out.println("Nilai tertinggi: " + tertinggi);
-        System.out.println("Nilai terendah " + terendah);
+        System.out.println("Nilai terendah: " + terendah);
+        
+        // Menampilkan jumlah mahasiswa yang lulus dan tidak lulus
+        System.out.println("Jumlah mahasiswa yang lulus: " + jumlahLulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + jumlahTidakLulus);
     }
 }
